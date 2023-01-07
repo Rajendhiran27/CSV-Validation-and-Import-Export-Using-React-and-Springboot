adcb473ff7c855e3ad3ee5aac0c6141589ed79f9
Rajendhiran27 <81033046+Rajendhiran27@users.noreply.github.com>
2023-01-07 18:05:05 +0530

    Add files via upload

---
+++ demo(backend)/src/main/java/com/example/FileUploadExceptionAdvice.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class FileUploadExceptionAdvice extends ResponseEntityExceptionHandler {
+
+  @SuppressWarnings("rawtypes")
+@ExceptionHandler(MaxUploadSizeExceededException.class)
+  public ResponseEntity handleMaxSizeException(MaxUploadSizeExceededException exc) {
+    return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage("File too large!",""));
+  }
+}
