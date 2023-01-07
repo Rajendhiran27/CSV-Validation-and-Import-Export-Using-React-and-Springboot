b6c191f261f605fb94b61c749ad220ea5dd04e95
Rajendhiran27 <81033046+Rajendhiran27@users.noreply.github.com>
2023-01-07 18:14:14 +0530

    Add files via upload

--- demo(backend)/src/main/java/com/example/demo/repo/RegisterRepository.java
+++ demo(backend)/src/main/java/com/example/demo/repo/RegisterRepository.java
@@ -1,1 +1,10 @@
+package com.example.demo.repo;
+import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
+import com.example.demo.model.Register;
 
+// @Repository
+public interface RegisterRepository extends JpaRepository<Register, Long>{
+    // Register findByEmailIdAndPassword(String emailId,String password);
+    Register findByEmailId(String emailId);
+}
